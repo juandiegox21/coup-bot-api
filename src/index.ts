e66ca9ef3d2b7cb419a57a4ff85959d0c0dd3002
Juandiegox21 <juandiegox17@gmail.com>
2022-09-03 16:21:02 -0400

    Add express server and prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import express from 'express'
 
 const prisma = new PrismaClient()
+const app = express()
 
-async function main() {
-  // ... your Prisma Client queries will go here
-}
+app.use(express.json())
 
-main()
-  .catch((e) => console.error(e))
-  .finally(async () => await prisma.$disconnect())
+// ... your REST API routes will go here
+app.get('/users', async (req, res) => {
+    const users = [
+        {
+            id: 1,
+            email:"example@prisma.io",
+            name:"example",
+        }
+    ];
+
+    res.json(users);
+  })
+
+app.listen(3000, () =>
+  console.log('REST API server ready at: http://localhost:3000'),
+)
