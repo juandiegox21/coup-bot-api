0af0626cf16d9c3fea2f0b13fb6bc3edc216655c
Juandiegox21 <juandiegox17@gmail.com>
2022-09-04 05:30:09 -0400

    Create seeders for cards

--- prisma/migrations/20220904065805_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Game" (
-    "id" SERIAL NOT NULL,
-    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "dateStarted" TIMESTAMP(3),
-    "dateEnded" TIMESTAMP(3),
-
-    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
-);
