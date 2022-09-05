8b6dac444c48e27fb2a41c6572e03a6e1a26e003
Juandiegox21 <juandiegox17@gmail.com>
2022-09-04 22:19:21 -0400

    add coup types

---
+++ src/types/coup.type.ts
@@ -0,0 +1,18 @@
+import { GamePlayer, GamePlayerCard } from "@prisma/client";
+
+export interface GamePlayerWithCard extends GamePlayer {
+    gamePlayerCard: Array<GamePlayerCard>
+}
+
+export type NumberOfCardsPerCharacter = {
+    Duke: number,
+    Contessa: number,
+    Ambassador: number,
+    Assassin: number,
+    Captain: number,
+}
+
+export type AssignedCards = {
+    gameId: number,
+    cardId: number,
+};
