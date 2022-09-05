f40eabf39deff10ba4380bfbdb8734f7ae32b7c0
Juandiegox21 <juandiegox17@gmail.com>
2022-09-05 16:33:49 -0400

    update game seeder

--- prisma/seeders/game.ts
+++ prisma/seeders/game.ts
@@ -1,7 +1,9 @@
+import moment from "moment";
+
 const gameData = () => ({
     dateCreated:  new Date(),
     dateStarted: null,
-    dateEnded: null,
+    dateEnded: moment(new Date()).utc().format('YYYY-MM-DD H:mm:ss'),
 });
 
 export default gameData;
