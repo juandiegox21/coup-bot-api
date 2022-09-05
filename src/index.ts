7c00b68366825ebb4818357751faae7c69750b6e
Juandiegox21 <juandiegox17@gmail.com>
2022-09-04 22:19:49 -0400

    import assigncards to player wip function

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,27 @@
 import { PrismaClient } from '@prisma/client'
 import express from 'express'
-import dealCards from '../helpers/dealCards';
+import assignCardsToPlayersWIP from './assignCardsToPlayers';
 
-const prisma = new PrismaClient()
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-async function testing() {
-    const gamePlayerResponse = await prisma.gamePlayer.findMany({
-        where: { gameId: 1 },
-        include: {
-            gamePlayerCard: true,
-        }
-    });
-
-    console.log(gamePlayerResponse[0].gamePlayerCard);
-
-    // dealCards(gamePlayers, cardsResponse);
-}
-
-testing();
+// assignCardsToPlayersWIP();
 
 // ... your REST API routes will go here
-// app.get('/users', async (req, res) => {
-//     const users = [
-//         {
-//             id: 1,
-//             email:"example@prisma.io",
-//             name:"example",
-//         }
-//     ];
+app.get('/users', async (req, res) => {
+    const users = [
+        {
+            id: 1,
+            email:"example@prisma.io",
+            name:"example",
+        }
+    ];
 
-//     res.json(users);
-// })
+    res.json(users);
+})
 
-// app.listen(3000, () =>
-//     console.log('REST API server ready at: http://localhost:3000'),
-// )
+app.listen(3000, () =>
+    console.log('REST API server ready at: http://localhost:3000'),
+)
